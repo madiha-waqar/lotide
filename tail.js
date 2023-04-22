63c44575414af229ec999a11745490a3738048b0
madiha-waqar <mail2madihawaqar@gmail.com>
2023-04-22 10:38:57 -0400

    tail.js

---
+++ tail.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log("✅✅✅" + "Assertion Passed: " + `${actual}` + " === " + `${expected}`) : console.log("🛑🛑🛑" + "Assertion Failed: " + `${actual}` + " !=== " + `${expected}`);
+};
+const tail = function(array) {
+  return array.slice(1);
+};
+
+//Test code
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
