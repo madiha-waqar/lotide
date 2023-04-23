6bfc73755d86e4e01bfc788d54167933ed99624a
madiha-waqar <mail2madihawaqar@gmail.com>
2023-04-23 17:31:19 -0400

    without.js

---
+++ without.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length)
+    return false;
+  else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1}  === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1}  !== ${array2}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let result = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i], 0)) {
+      result.push(source[i]);
+    }
+  }
+  return result;
+};
+
+assertArraysEqual(without(["a", "b", "c"], ["c"]), ["a", "b"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without(["hello", "world", "lighthouse"], ["lighthouse"]), ["hello", "world"]);
+
