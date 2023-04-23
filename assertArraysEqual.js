d91cdab98e74a964d4a974d76ad9fb8554b77052
madiha-waqar <mail2madihawaqar@gmail.com>
2023-04-23 15:15:31 -0400

    assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function(array1, array2) {
+    if (array1.length !== array2.length)
+      return false;
+    else {
+      for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  };
+  
+  const assertArraysEqual = function(array1, array2) {
+    if (eqArrays(array1, array2)) {
+      console.log(`✅✅✅ Assertion Passed: ${array1}  === ${array2}`); 
+        } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${array1}  !== ${array2}`); 
+    }
+  };
