a409e107d9a565ac87ef301da7f9dcc0e0df14cb
madiha-waqar <mail2madihawaqar@gmail.com>
2023-04-30 22:04:25 -0400

    findKey.js

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  actual === expected ? console.log("✅✅✅" + "Assertion Passed: " + `${actual}` + " === " + `${expected}`) :
+    console.log("🛑🛑🛑" + "Assertion Failed: " + `${actual}` + " !=== " + `${expected}`);
+};
+
+const findKey = function (obj, callback) {
+  const objKeys = Object.keys(obj);
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return key
+    }
+  }
+};
+
+assertEqual(findKey({
+  'Fiat': {model:100, color:"white"},
+  'Lexus': {model:250, color:"red"},
+  'Toyota': {model:500, color:"blue"},
+  'Ford': {model:1000, color:"black"},
+}, car => car.model === 1000), 'Ford');
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), 'noma');
