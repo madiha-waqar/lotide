2204d0fadc51307aa70142626a83872ee95bdf99
madiha-waqar <mail2madihawaqar@gmail.com>
2023-05-11 12:14:22 -0400

    Refactoring wrt require and export

--- middle.js
+++ middle.js
@@ -1,23 +1,26 @@
-const eqArrays = function(array1, array2) {
-    if (array1.length !== array2.length)
-      return false;
-    else {
-      for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-          return false;
-        }
-      }
-      return true;
-    }
-  };
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
+
+// const eqArrays = function(array1, array2) {
+//     if (array1.length !== array2.length)
+//       return false;
+//     else {
+//       for (let i = 0; i < array1.length; i++) {
+//         if (array1[i] !== array2[i]) {
+//           return false;
+//         }
+//       }
+//       return true;
+//     }
+//   };
   
-  const assertArraysEqual = function(array1, array2) {
-    if (eqArrays(array1, array2)) {
-      console.log(`✅✅✅ Assertion Passed: ${array1}  === ${array2}`); 
-        } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${array1}  !== ${array2}`); 
-    }
-  };
+//   const assertArraysEqual = function(array1, array2) {
+//     if (eqArrays(array1, array2)) {
+//       console.log(`✅✅✅ Assertion Passed: ${array1}  === ${array2}`); 
+//         } else {
+//       console.log(`🛑🛑🛑 Assertion Failed: ${array1}  !== ${array2}`); 
+//     }
+//   };
 
   const middle = function (array){
     let arrayLength = array.length;
@@ -32,12 +35,16 @@ const eqArrays = function(array1, array2) {
     else {
       return [array[mid - 1], array[mid]] 
     }
+    return;
   }
+module.exports = middle
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+//TEST CODE
+// console.log(middle([1])); // => []
+// console.log(middle([1, 2])); // => []
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
---
+++ test/middleTest.js
@@ -0,0 +1,10 @@
+const  middle = require('../middle')
+
+//TEST CODE
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
